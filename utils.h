e5c06af964b239c656ba2438d568cbcbfe8d71d1
Sergio Guastaferro <sergio.guastaferro@yahoo.it>
2026-01-09 09:48:26 +0100

    fix include header only on ISA vect enabled

--- utils.h
+++ utils.h
@@ -1,7 +1,7 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
-#ifdef __riscv
+#if defined(__riscv) && defined(__riscv_vector)
 #include <riscv_vector.h>
 #endif
 
@@ -11,7 +11,7 @@ void print_matrix(double* M, int size);
 
 void print_matrixf32(float* M, int tile_size, int size, int print_inline);
 
-#ifdef __riscv
+#if defined(__riscv) && defined(__riscv_vector)
 void print_vmatrixf32(int size, vfloat32m1_t c1, vfloat32m1_t c2);
 void print_vmatrixf32_4x4(vfloat32m1_t c1, vfloat32m1_t c2, vfloat32m1_t c3, vfloat32m1_t c4);
 #endif
